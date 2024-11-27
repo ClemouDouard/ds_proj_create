812779a47bbe94c432bb208d97099caf3c652441
ClemouDouard <clementleveque911@gmail.com>
2024-11-27 23:47:38 +0100

    added virtual environment creation and requirements installation

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
+import subprocess as sub
 
-# Define the project structure
-def create_project_structure(project_name):
+def create_project_structure(project_name: str) -> None:
     structure = {
         f"{project_name}/": [
             "data/raw/",
@@ -17,13 +17,11 @@ def create_project_structure(project_name):
         ]
     }
 
-    # Create directories
     for base, folders in structure.items():
         for folder in folders:
             path = os.path.join(base, folder)
             os.makedirs(path, exist_ok=True)
 
-    # Create essential files
     essential_files = [
         f"{project_name}/README.md",
         f"{project_name}/requirements.txt",
@@ -32,7 +30,7 @@ def create_project_structure(project_name):
     for file in essential_files:
         with open(file, "w") as f:
             if "README.md" in file:
-                f.write(f"# {project_name}\n\nProject description goes here.\n")
+                f.write(f"# {project_name}\n\nProject description goes here.\n # Usage\n\n To activate the virtual environment run \n```bash\n source {project_name}/my_env/bin/activate\n```")
             if "requirements.txt" in file:
                 f.write(
                     "numpy\n"
@@ -46,7 +44,18 @@ def create_project_structure(project_name):
 
     print(f"Project structure for '{project_name}' created successfully!")
 
-# Call the function
+def virtual(project_name: str) -> None:
+    env_path = f"{project_name}/my_env"
+    sub.run(["python3", "-m", "venv", env_path])
+
+    pip_path = os.path.join(env_path, "bin", "pip")
+    
+    sub.run([pip_path, "install", "-r", f"{project_name}/requirements.txt"])
+
+    print(f"L'environnement virtuel a été créé dans '{env_path}'.")
+    print(f"Pour l'activer, exécutez : source {env_path}/bin/activate")
+
 if __name__ == "__main__":
     project_name = input("Enter the project name: ").strip()
     create_project_structure(project_name)
+    virtual(project_name)
